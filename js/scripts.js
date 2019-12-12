5b809404e466355b0d6a34ca13072ca08130cda5
jennifer <57957111+jecanderson@users.noreply.github.com>
2019-12-12 16:37:04 -0500

    js coding
    added js file and imbedded coding

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi! Nice to see you! Welcome to my portfolio!");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += 'responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
